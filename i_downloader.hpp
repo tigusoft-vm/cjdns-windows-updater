0972ff4f20ca18a63cd4dd71c9b6c80b65fcabad
rob <rob@mail.l>
2015-12-17 12:12:33 +0000

    comments

--- i_downloader.hpp
+++ i_downloader.hpp
@@ -6,9 +6,12 @@
 class i_downloader
 {
 	public:
+		/**
+		 * Download file from @param file_address and save as @param out_path
+		 */
 		virtual void download_file(const std::string &file_address, const std::string &out_path) = 0;
 		virtual ~i_downloader() = default;
 };
 
-#endif // C_DOWNLOADER_H
+#endif // I_DOWNLOADER_H
 
