52d370accafb0c75bfb3717828b0f67c5c3d32af
rob <rob@mail.l>
2015-12-18 09:59:41 +0000

    generate request

--- c_url_downloader.cpp
+++ c_url_downloader.cpp
@@ -1,10 +1,10 @@
 #include "c_url_downloader.hpp"
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 #include <boost/asio.hpp>
 
-void c_url_downloader::download_file (const std::string &file_address, const std::string &out_path)
-{
+void c_url_downloader::download_file (const std::string &file_address, const std::string &out_path) {
 	using namespace boost::asio;
 
 	io_service io_servise;
@@ -13,14 +13,25 @@ void c_url_downloader::download_file (const std::string &file_address, const std
 		ip::tcp::resolver resolver(io_servise);
 		ip::tcp::resolver::query query(file_address, "http");
 		endpoint_iterator = resolver.resolve(query);
+		ip::tcp::socket socket = ip::tcp::socket(io_servise);
+		boost::asio::connect(socket, resolver.resolve(query));
+
 	}
 	catch(boost::system::system_error& error) {
-		std::cerr << "Resolver error: " << error.what() << std::endl;
+		std::cerr << "Error: " << error.what() << std::endl;
 		return;
 	}
 
-	ip::tcp::socket socket(io_service);
 
+	std::ofstream out_file(out_path); // TODO save data to file
+}
 
-	std::ofstream out_file(out_path); // TODO
+std::string c_url_downloader::generate_request(const std::string &hostname, const std::string &path) {
+	std::ostringstream oss;
+	oss
+		<< "GET " << path << " HTTP/1.1\r\n"
+		<< "Host: " << hostname << "\r\n"
+		<< "Connection: close" << "\r\n"
+		<< "\r\n";
+	return oss.str();
 }
--- c_url_downloader.hpp
+++ c_url_downloader.hpp
@@ -7,6 +7,8 @@ class c_url_downloader :  i_downloader
 {
 	public:
 		virtual void download_file(const std::string &file_address, const std::string &out_path) override;
+	private:
+		std::string generate_request(const std::string& hostname, const std::string& path);
 };
 
 #endif // C_URL_DOWNLOADER_H
