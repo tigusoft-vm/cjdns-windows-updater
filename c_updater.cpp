2d44760b6eac3e18a27e6ef6cc99b13658b728d1
rob <rob@mail.l>
2015-12-28 10:08:33 +0000

    remove old update()

--- c_updater.cpp
+++ c_updater.cpp
@@ -10,16 +10,16 @@ c_updater::c_updater() :
 }
 
 void c_updater::update() {
-	// load file
-	const std::string file_path("test.txt"); // TODO filename
-	std::ifstream file(file_path, std::ios::binary | std::ios::ate);
-	size_t file_size =  file.tellg();
-	file.close();
-	std::cout << "file_size: " << file_size << std::endl;
-
-	std::string binary_data_from_file;
-	binary_data_from_file.reserve(file_size);
-
-	file.open(file_path, std::ios::binary | std::ios::in);
-	file.read(&binary_data_from_file[0], file_size);
+// 	// load file
+// 	const std::string file_path("test.txt"); // TODO filename
+// 	std::ifstream file(file_path, std::ios::binary | std::ios::ate);
+// 	size_t file_size =  file.tellg();
+// 	file.close();
+// 	std::cout << "file_size: " << file_size << std::endl;
+//
+// 	std::string binary_data_from_file;
+// 	binary_data_from_file.reserve(file_size);
+//
+// 	file.open(file_path, std::ios::binary | std::ios::in);
+// 	file.read(&binary_data_from_file[0], file_size);
 }
--- c_updater.hpp
+++ c_updater.hpp
@@ -13,6 +13,8 @@ class c_updater
 		void update();
 	private:
 		std::unique_ptr<i_downloader> m_downloader;
+		unsigned int get_remote_version(); ///< @returns version on server
+		unsigned int get_local_version(); ///< @returns local version
 };
 
 #endif // C_UPDATER_H
