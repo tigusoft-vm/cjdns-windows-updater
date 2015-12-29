3d7da9c306203e5bac6e6e1e5992b65c84803f95
rob <rob@mail.l>
2015-12-29 12:24:20 +0000

    remove sha512

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
 #include "c_http_downloader.hpp"
-#include "c_sha512_ext.hpp"
 #include "c_updater.hpp"
 
 #include <iostream>
 #include <fstream>
-
-bool sha512test(const std::string msg) {
-
-    std::string hash = c_sha512_ext::generate_hash(msg);
-
-    std::cout << "sha512 test:\n";
-    std::cout << "msg: " << msg << "\nsha512: " << hash << std::endl;
-    std::cout << "verifying . . .\n";
-    if(c_sha512_ext::verify_hash(msg,hash)) {
-        std::cout << "OK";
-        return 0;
-    } else {
-        std::cout << "BAD SUM";
-        return 1;
-    }
-}
 
 int main() {
 	std::ofstream ofs("out");
@@ -28,6 +11,5 @@ int main() {
 	c_updater updater;
 	updater.update();
 
-	sha512test("test");
     return 0;
 }
