54b279f327e93c12d68b5792cf7f215050ecc481
rob <rob@mail.l>
2015-12-29 12:21:44 +0000

    build src to lib

--- gui/updater-gui/main.cpp
+++ gui/updater-gui/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "c_updater.hpp"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -6,6 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+    c_updater updater;
     return a.exec();
 }
