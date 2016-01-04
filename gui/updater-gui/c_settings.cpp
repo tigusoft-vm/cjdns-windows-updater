f07c4cb5b858a94616b757ec54c02d38e7533742
rob <rob@mail.l>
2016-01-04 15:03:05 +0000

    example read language from settings file

--- gui/updater-gui/c_settings.cpp
+++ gui/updater-gui/c_settings.cpp
@@ -1,4 +1,6 @@
 #include "c_settings.hpp"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 c_settings::c_settings()
 {
@@ -7,4 +9,16 @@ c_settings::c_settings()
 c_settings &c_settings::getInstance() {
     static c_settings instance;
     return instance;
+}
+
+void c_settings::load_settings(const std::string &file_path) {
+    using namespace boost::property_tree;
+    ptree tree;
+    read_xml(file_path, tree);
+    tree.get("settings.lang", language);
+}
+
+void c_settings::save_settings(const std::string &file_path) {
+    using namespace boost::property_tree;
+    ptree tree;
 }
--- gui/updater-gui/c_settings.hpp
+++ gui/updater-gui/c_settings.hpp
@@ -1,6 +1,8 @@
 #ifndef C_SETTINGS_HPP
 #define C_SETTINGS_HPP
 
+#include <string>
+
 // singleton
 class c_settings
 {
@@ -11,8 +13,11 @@ class c_settings
         void operator= (const c_settings &) = delete;
 
         static c_settings& getInstance();
+
+        std::string language;
     private:
-        void load_settings();
+        void load_settings(const std::string &file_path);
+        void save_settings(const std::string &file_path);
 };
 
 #endif // C_SETTINGS_HPP
