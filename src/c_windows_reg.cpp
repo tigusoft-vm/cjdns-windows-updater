80103a53466f5754a78923c0380c7acc6c19979e
win <win@example.com>
2016-02-18 17:22:45 +0100

    [win utility] register functions

--- src/c_windows_reg.cpp
+++ src/c_windows_reg.cpp
@@ -1,7 +1,21 @@
 #if defined _WIN32 || defined __CYGWIN__
 
 #include "c_windows_reg.hpp"
+#include "c_windows_utility.hpp"
 #include <stdexcept>
+
+#ifdef __CYGWIN__
+#include <sstream>
+namespace std {
+template <typename T>
+std::string to_string(T val) {
+    std::stringstream stream;
+    stream << val;
+    return stream.str();
+}
+} // namespace std
+#endif
+
 
 namespace win_utility {
 
@@ -25,6 +39,23 @@ std::string c_windows_reg::get_register_value(HKEY root, const std::string &key,
 
 	RegCloseKey(hKey);
 	return value;
+}
+
+void c_windows_reg::set_register_value(HKEY root, const std::string &key, const std::string &name, DWORD type, const BYTE *data, DWORD size_of_data) {
+	if (!c_windows_utility::is_user_admin())
+		throw std::runtime_error("required administrator permissions");
+	HKEY hKey;
+	DWORD dwFunc;
+	LONG  ret;
+
+	ret = RegCreateKeyEx(root, key.c_str(), 0, nullptr, 0, KEY_WRITE, NULL, &hKey, &dwFunc);
+	if(ret != ERROR_SUCCESS)
+		throw std::runtime_error("RegCreateKeyEx returns " + std::to_string(ret));
+
+	// set register value
+	ret = RegSetValueEx(hKey, name.c_str(), 0, type, data, size_of_data);
+	if(ret != ERROR_SUCCESS)
+		throw std::runtime_error("RegSetValueEx returns " + std::to_string(ret));
 }
 
 } // namespace
--- src/c_windows_reg.hpp
+++ src/c_windows_reg.hpp
@@ -28,8 +28,43 @@ namespace win_utility {
 			 */
 			static std::string get_register_value(HKEY root, const std::string &key, const std::string &name);
 			
-			
-			static void set_register_value();
+			/**
+			 * @param root 
+			 * use one of predefined keys:
+			 * HKEY_CLASSES_ROOT
+			 * HKEY_CURRENT_CONFIG
+			 * HKEY_CURRENT_USER
+			 * HKEY_LOCAL_MACHINE
+			 * HKEY_USERS
+			 * 
+			 * @param key 
+			 * path of register key without root name
+			 *
+			 * @param name
+			 * name of the registry subkey to be opened
+			 * 
+			 * @param type
+			 * type of register value
+			 * https://msdn.microsoft.com/en-us/library/windows/desktop/ms724884
+			 * 
+			 * @param data
+			 * the data to be stored
+			 * For string-based types, such as REG_SZ, the string must be null-terminated. With
+			 * the REG_MULTI_SZ data type, the string must be terminated with two null characters.
+			 * String-literal values must be formatted using a backslash preceded by another backslash
+			 * as an escape character. For example, specify "C:\\mydir\\myfile" to store the string
+			 * "C:\mydir\myfile".
+			 * 
+			 * @param size_of_data
+			 * The size of the information pointed to by the lpData parameter, in bytes. If the data is
+			 * of type REG_SZ, REG_EXPAND_SZ, or REG_MULTI_SZ, cbData must include the size of the
+			 * terminating null character or characters.
+			 *
+			 * @throw std::runtime_error if error
+			 * 
+			 * This function requires administrator permissions. Else throw std::runtime_error
+			 */
+			static void set_register_value(HKEY root, const std::string &key, const std::string &name, DWORD type, const BYTE *data, DWORD size_of_data);
 	};
 	
 } // namespace
