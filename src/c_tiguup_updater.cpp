5e107ae333c376bb0b2b82e08c1711d07bef0477
win <win@example.com>
2016-02-26 13:57:10 +0100

    get updater version from register

--- src/c_tiguup_updater.cpp
+++ src/c_tiguup_updater.cpp
@@ -1,9 +1,21 @@
 #include "c_tiguup_updater.hpp"
+#include "../win-utility-lib/c_windows_reg.hpp"
+
+#include <iostream> // XXX
 
 c_tiguup_updater::c_tiguup_updater() {
-	
+	std::cout << get_local_version() << std::endl;
 }
 
 void c_tiguup_updater::update() {
 	
+}
+
+// updater version saved in HKEY_LOCAL_MACHINE\SOFTWARE\cjdns in updater-ver
+unsigned int c_tiguup_updater::get_local_version() {
+	std::string ver(
+		win_utility::c_windows_reg::get_register_value(HKEY_LOCAL_MACHINE, R"(SOFTWARE\cjdns)", "updater-ver") 
+	);
+	unsigned int ret_ver = *reinterpret_cast<const DWORD*>(ver.data());
+	return ret_ver;
 }
--- src/c_tiguup_updater.hpp
+++ src/c_tiguup_updater.hpp
@@ -7,6 +7,8 @@ class c_tiguup_updater : public c_updater {
 	public:
 		c_tiguup_updater();
 		virtual void update() override;
+	private:
+		 unsigned int get_local_version() override;
 };
 
 #endif
