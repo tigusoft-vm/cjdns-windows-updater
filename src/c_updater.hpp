a05174a413fd79eb9af8bd3c685d777054e56306
win <win@example.com>
2016-03-04 11:46:59 +0100

    [doc] c_updater

--- src/c_updater.hpp
+++ src/c_updater.hpp
@@ -17,13 +17,16 @@ class c_updater {
 		virtual ~c_updater() = default;
 	protected:
 		std::unique_ptr<i_downloader> m_downloader;
-		virtual unsigned int get_remote_version() = 0; ///< @returns version on server (server_address/ver)
+		virtual unsigned int get_remote_version() = 0; ///< @returns version on server
 		virtual unsigned int get_local_version() = 0; ///< @returns local version
 
 		/**
-		 * Remove non digit chars from @param version_str
-		 * for @param version_str == 17.1-4-test returns 1714
+		 * Remove non digit chars from version_str
+		 * i.e. for version_str == 17.1-4-test returns 1714
+		 * @param version_str version as string
 		 * @throw std::invalid_argument if no digits in @param version_str
+		 * \par Exception safety
+		 * strong exception guarantee
 		 */
 		virtual unsigned int get_version_number(std::string &&version_str);
 		const std::string m_server_address;
--- win-utility-lib/c_windows_service.hpp
+++ win-utility-lib/c_windows_service.hpp
@@ -22,7 +22,8 @@ class c_windows_service {
 		 * stop service @param service_name
 		 * @throw std::runtime_error if error
 		 * This function requires administrator permissions. Else throw std::runtime_error
-		 * Exception safety: strong exception guarantee
+		 * \par Exception safety 
+		 * strong exception guarantee
 		 */
 		static void start_service(const std::string &service_name);
 };
